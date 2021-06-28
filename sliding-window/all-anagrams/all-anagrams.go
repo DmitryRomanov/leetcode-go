b68bf6635b7859f6b79c1d95a8ba53267e2a1e2b
Dmitry Romanov <dmitry.romanov85@gmail.com>
2021-06-28 10:55:27 +0700

    anagrams equals

--- sliding-window/all-anagrams/all-anagrams.go
+++ sliding-window/all-anagrams/all-anagrams.go
@@ -1,19 +1,23 @@
 package allanagrams
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 func findAnagrams(s string, p string) []int {
 	left := 0
-	right := len(p) - 1
+	right := len(p)
 	var result []int
 	stringChars := []rune(s)
 	sampleChars := RuneSlice([]rune(p))
 	sort.Sort(sampleChars)
 
-	for right < len(p) {
+	for right < len(stringChars) {
 		window := RuneSlice(stringChars[left:right])
 		sort.Sort(window)
-		if window == sampleChars {
+		fmt.Println()
+		if sampleChars.Equal(window) {
 
 		}
 	}
@@ -25,3 +29,5 @@ type RuneSlice []rune
 func (p RuneSlice) Len() int           { return len(p) }
 func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
 func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func (p RuneSlice) Equal(sample RuneSlice) bool { return false }
