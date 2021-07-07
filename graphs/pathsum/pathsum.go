7ad59a81e0dec3dd04bebd96ac5f397b61563f4d
Dmitry Romanov <dmitry.romanov85@gmail.com>
2021-07-07 10:39:57 +0700

    pathsum common cases

--- graphs/pathsum/pathsum.go
+++ graphs/pathsum/pathsum.go
@@ -1,8 +1,33 @@
 package pathsum
 
-/**
- * Definition for a binary tree node.
- */
 func hasPathSum(root *TreeNode, targetSum int) bool {
-	return true
+	if root == nil {
+		return false
+	}
+
+	childrenValue := targetSum - root.Val
+
+	if childrenValue == 0 && (root.Left != nil || root.Right != nil) {
+		return false
+	}
+
+	if root.Left != nil {
+		leftSum := hasPathSum(root.Left, childrenValue)
+		if leftSum {
+			return true
+		}
+	}
+
+	if root.Right != nil {
+		rightSum := hasPathSum(root.Right, childrenValue)
+		if rightSum {
+			return true
+		}
+	}
+
+	if root.Val == targetSum {
+		return true
+	} else {
+		return false
+	}
 }
--- graphs/pathsum/pathsum_test.go
+++ graphs/pathsum/pathsum_test.go
@@ -7,6 +7,140 @@ import (
 
 func TestHasPathSumExample1(t *testing.T) {
 	assert := assert.New(t)
-	result := hasPathSum(&TreeNode{}, 22)
+	result := hasPathSum(&TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 11,
+				Left: &TreeNode{
+					Val: 7,
+				},
+				Right: &TreeNode{
+					Val: 2,
+				},
+			},
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val: 8,
+			Left: &TreeNode{
+				Val: 13,
+			},
+			Right: &TreeNode{
+				Val: 4,
+				Right: &TreeNode{
+					Val: 1,
+				},
+			},
+		},
+	}, 22)
+	assert.True(result)
+}
+
+func TestHasPathSumExample2(t *testing.T) {
+	assert := assert.New(t)
+	result := hasPathSum(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+		Right: &TreeNode{
+			Val: 3,
+		},
+	}, 5)
+	assert.False(result)
+}
+
+func TestHasPathSumExample3(t *testing.T) {
+	assert := assert.New(t)
+	result := hasPathSum(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+	}, 0)
+	assert.False(result)
+}
+
+func TestHasPathSumExample4(t *testing.T) {
+	assert := assert.New(t)
+	result := hasPathSum(&TreeNode{}, 1)
+	assert.False(result)
+}
+
+func TestHasPathSumExample5(t *testing.T) {
+	assert := assert.New(t)
+	result := hasPathSum(nil, 1)
+	assert.False(result)
+}
+
+func TestHasPathSumExample6(t *testing.T) {
+	assert := assert.New(t)
+	result := hasPathSum(&TreeNode{
+		Val: 2,
+		Left: &TreeNode{
+			Val: 1,
+		},
+	}, 1)
+	assert.False(result)
+}
+
+func TestHasPathSumExample7(t *testing.T) {
+	assert := assert.New(t)
+	result := hasPathSum(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+	}, 1)
+	assert.False(result)
+}
+
+func TestHasPathSumExample8(t *testing.T) {
+	assert := assert.New(t)
+	result := hasPathSum(&TreeNode{
+		Val: -2,
+		Right: &TreeNode{
+			Val: -3,
+		},
+	}, -5)
+	assert.True(result)
+}
+
+func TestHasPathSumExample9(t *testing.T) {
+	assert := assert.New(t)
+	result := hasPathSum(&TreeNode{
+		Val: -2,
+		Right: &TreeNode{
+			Val: -3,
+		},
+	}, -2)
+	assert.False(result)
+}
+
+func TestHasPathSumExample10(t *testing.T) {
+	assert := assert.New(t)
+	result := hasPathSum(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: -2,
+			Left: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: -1,
+				},
+			},
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+		Right: &TreeNode{
+			Val: -3,
+			Left: &TreeNode{
+				Val: -2,
+			},
+		},
+	}, -1)
 	assert.True(result)
 }
