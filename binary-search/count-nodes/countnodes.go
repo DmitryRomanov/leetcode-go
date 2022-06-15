bda702e17107238e3d550e2fb8d85b125ff49ce1
Dmitry Romanov <dmitry.romanov85@gmail.com>
2022-06-15 20:22:17 +0700

    first resolve count nodes

--- binary-search/count-nodes/countnodes.go
+++ binary-search/count-nodes/countnodes.go
@@ -1,5 +1,20 @@
 package countnodes
 
 func countNodes(root *TreeNode) int {
+	if root.Right != nil {
+		return 2 * (1 + calculateByLeftNodes(root.Right))
+	}
+
+	if root.Val != 0 {
+		return 1
+	}
+
 	return 0
+}
+
+func calculateByLeftNodes(root *TreeNode) int {
+	if root.Left != nil {
+		return 1 + calculateByLeftNodes(root.Left)
+	}
+	return 1
 }
