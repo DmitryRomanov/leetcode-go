3989f1a83ebb3285ca25a64bb14c45a5fdef8982
Dmitry Romanov <dmitry.romanov85@protonmail.com>
2022-07-05 10:36:27 +0700

    use findClosestNonOverlapping

--- dynamic/two-best-non-overlapping-events/two-best-non-overlapping-events.go
+++ dynamic/two-best-non-overlapping-events/two-best-non-overlapping-events.go
@@ -10,35 +10,41 @@ type event struct {
 
 func maxTwoEvents(events_array [][]int) int {
 	sort.Slice(events_array, func(i, j int) bool {
-		return events_array[i][1] < events_array[j][1]
+		return events_array[i][0] < events_array[j][0]
 	})
 
 	max := 0
 	events := make([]event, len(events_array))
 	for i := 0; i < len(events_array); i++ {
-		tmpEvent := event{
+		events[i] = event{
 			startTime: events_array[i][0],
 			endTime:   events_array[i][1],
 			value:     events_array[i][2],
 		}
-		events = append(events, tmpEvent)
 	}
 
-	for left := 0; left < len(events); left++ {
-		if events[left].value > max {
-			max = events[left].value
-		}
-		for right := left + 1; right < len(events); right++ {
-			leftEvent := events[left]
-			rightEvent := events[right]
-			if leftEvent.endTime < rightEvent.startTime {
-				tmpMax := leftEvent.value + rightEvent.value
-				if tmpMax > max {
-					max = tmpMax
-				}
-			}
+	for i, event := range events {
+		idxOfClosestEvent := findClosestNonOverlapping(events, event.endTime, event.endTime, len(events)-1)
+		tmpMax := events[i].value + events[idxOfClosestEvent].value
+		if tmpMax > max {
+			max = tmpMax
 		}
 	}
 
 	return max
+}
+
+func findClosestNonOverlapping(events []event, end, left, right int) int {
+	if right > left {
+		return left
+	}
+	median := (left + right) / 2
+
+	if events[median].endTime > end {
+		return findClosestNonOverlapping(events, end, median+1, right)
+	} else if events[median].endTime < end {
+		return findClosestNonOverlapping(events, end, left, median-1)
+	} else {
+		return median
+	}
 }
