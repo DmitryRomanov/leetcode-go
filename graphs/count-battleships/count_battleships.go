f1c0725dce86957d53218f942a529d16f42ffce5
Dmitry Romanov <dmitry.romanov85@gmail.com>
2022-07-20 19:06:16 +0700

    first resolve for battleship problem

--- graphs/count-battleships/count_battleships.go
+++ graphs/count-battleships/count_battleships.go
@@ -1,5 +1,13 @@
 package count_battleships
 
-func countBattleships(board [][]byte) int {
-	return 0
+func countBattleships(board [][]byte) (k int) {
+	for i := range board {
+		for j := range board[i] {
+			if board[i][j] == 'X' && ((i == 0 || board[i-1][j] != 'X') && (j == 0 || board[i][j-1] != 'X')) {
+				k++
+			}
+		}
+	}
+
+	return
 }
