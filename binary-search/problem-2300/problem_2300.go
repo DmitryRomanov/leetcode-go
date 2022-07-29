e4b51d58f5455ee3c91bee3828708fd9a3a4de1f
Dmitry Romanov <dmitry.romanov85@gmail.com>
2022-07-29 20:00:27 +0700

    problem 2300 with binary search

--- binary-search/problem-2300/problem_2300.go
+++ binary-search/problem-2300/problem_2300.go
@@ -1,5 +1,37 @@
 package problem_2300
 
+import (
+	"math"
+	"sort"
+)
+
 func successfulPairs(spells []int, potions []int, success int64) []int {
-	return []int{}
+	result := []int{}
+	sort.Ints(potions)
+
+	for i := range spells {
+		minSuccessPotion := math.Ceil(float64(success) / float64(spells[i]))
+		greaterIndex := indexGreaterThanValue(potions, int(minSuccessPotion))
+		result = append(result, len(spells)-greaterIndex)
+	}
+	return result
+}
+
+func indexGreaterThanValue(potions []int, searchValue int) int {
+	left := 0
+	right := len(potions) - 1
+
+	median := (left + right) / 2
+
+	for right >= left {
+		median = (left + right) / 2
+		if potions[median] < searchValue {
+			right = median - 1
+		} else if potions[median] > searchValue {
+			left = median + 1
+		} else {
+			return median
+		}
+	}
+	return median
 }
--- binary-search/problem-2300/problem_2300_test.go
+++ binary-search/problem-2300/problem_2300_test.go
@@ -1,8 +1,9 @@
 package problem_2300
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSuccessfulPairsExample1(t *testing.T) {
