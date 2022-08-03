4b7d1427824deea6078e073ce785c4ea582db726
Dmitry Romanov <dmitry.romanov85@gmail.com>
2022-08-03 09:14:58 +0700

    some optimizations for problem 2300

--- binary-search/problem-2300/problem_2300.go
+++ binary-search/problem-2300/problem_2300.go
@@ -6,16 +6,16 @@ import (
 )
 
 func successfulPairs(spells []int, potions []int, success int64) []int {
-	result := []int{}
+	result := make([]int, len(spells))
 	sort.Ints(potions)
 
-	for i := range spells {
-		minSuccessPotion := math.Ceil(float64(success) / float64(spells[i]))
+	for i, spell := range spells {
+		minSuccessPotion := math.Ceil(float64(success) / float64(spell))
 		greaterIndex := indexGreaterThanValue(potions, int(minSuccessPotion))
 		if greaterIndex != -1 {
-			result = append(result, len(potions)-greaterIndex)
+			result[i] = len(potions) - greaterIndex
 		} else {
-			result = append(result, 0)
+			result[i] = 0
 		}
 	}
 	return result
@@ -25,10 +25,10 @@ func indexGreaterThanValue(potions []int, searchValue int) int {
 	left := 0
 	right := len(potions) - 1
 
-	median := (left + right) / 2
+	median := left + (right-left)>>1
 	result := -1
 	for right >= left {
-		median = (left + right) / 2
+		median = left + (right-left)>>1
 		if potions[median] >= searchValue {
 			right = median - 1
 			result = median
