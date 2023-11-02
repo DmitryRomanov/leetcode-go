30fc0d097c8aa797d5579a412730946250586f32
Dmitry Romanov <dmitry.romanov85@gmail.com>
2023-11-02 11:23:43 +0700

    01-matrix

---
+++ array/01-matrix/01-matrix.go
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/01-matrix/description/
+package matrix_01
+
+import "math"
+
+func updateMatrix(mat [][]int) [][]int {
+	result := [][]int{}
+	queue := [][]int{}
+	for i := range mat {
+		i_result := []int{}
+		for j := range mat[i] {
+			if mat[i][j] == 0 {
+				i_result = append(i_result, 0)
+			} else {
+				i_result = append(i_result, math.MaxInt32)
+				queue = append(queue, []int{i, j})
+			}
+		}
+		result = append(result, i_result)
+	}
+
+	var current []int
+
+	for len(queue) > 0 {
+		current, queue = queue[0], queue[1:]
+		cur_i, cur_j := current[0], current[1]
+
+		directions := [][2]int{
+			{cur_i, cur_j + 1},
+			{cur_i, cur_j - 1},
+			{cur_i + 1, cur_j},
+			{cur_i - 1, cur_j},
+		}
+		for _, direction := range directions {
+			i := direction[0]
+			j := direction[1]
+			if i >= 0 && j >= 0 && i < len(mat) && j < len(mat[i]) {
+				result_in_new_direction := result[i][j] + 1
+				current_result := result[cur_i][cur_j]
+
+				if result_in_new_direction < current_result {
+					result[cur_i][cur_j] = result_in_new_direction
+					queue = append(queue, []int{i, j})
+				}
+			}
+
+		}
+	}
+
+	return result
+}
---
+++ array/01-matrix/01-matrix_test.go
@@ -0,0 +1,61 @@
+package matrix_01
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExample1(t *testing.T) {
+	assert := assert.New(t)
+	result := updateMatrix([][]int{
+		{0, 0, 0},
+		{0, 1, 0},
+		{0, 0, 0},
+	})
+	assert.Equal([][]int{
+		{0, 0, 0},
+		{0, 1, 0},
+		{0, 0, 0},
+	}, result)
+}
+
+func TestExample2(t *testing.T) {
+	assert := assert.New(t)
+	result := updateMatrix([][]int{
+		{0, 0, 0},
+		{0, 1, 0},
+		{1, 1, 1},
+	})
+	assert.Equal([][]int{
+		{0, 0, 0},
+		{0, 1, 0},
+		{1, 2, 1},
+	}, result)
+}
+
+func TestExample3(t *testing.T) {
+	assert := assert.New(t)
+	result := updateMatrix([][]int{
+		{1, 1, 1},
+		{0, 1, 0},
+		{0, 0, 0},
+	})
+	assert.Equal([][]int{
+		{1, 2, 1},
+		{0, 1, 0},
+		{0, 0, 0},
+	}, result)
+}
+
+func TestExample4(t *testing.T) {
+	assert := assert.New(t)
+	result := updateMatrix([][]int{
+		{1, 1},
+		{1, 0},
+	})
+	assert.Equal([][]int{
+		{2, 1},
+		{1, 0},
+	}, result)
+}
