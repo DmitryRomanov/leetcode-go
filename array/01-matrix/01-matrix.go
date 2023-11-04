2f476051e1be1a1b0a5cc47da83b72415342e847
Dmitry Romanov <dmitriyro@ozon.ru>
2023-11-04 10:47:36 +0700

    perf tests for 01-matrix

--- array/01-matrix/01-matrix.go
+++ array/01-matrix/01-matrix.go
@@ -9,9 +9,9 @@ func updateMatrix(mat [][]int) [][]int {
 		for j := range mat[i] {
 			if mat[i][j] == 0 {
 				i_result = append(i_result, 0)
+				queue = append(queue, []int{i, j})
 			} else {
 				i_result = append(i_result, len(mat))
-				queue = append(queue, []int{i, j})
 			}
 		}
 		result = append(result, i_result)
@@ -33,15 +33,59 @@ func updateMatrix(mat [][]int) [][]int {
 			i := direction[0]
 			j := direction[1]
 			if i >= 0 && j >= 0 && i < len(mat) && j < len(mat[i]) {
-				result_in_new_direction := result[i][j] + 1
-				current_result := result[cur_i][cur_j]
+				result_in_new_direction := result[i][j]
+				current_result := result[cur_i][cur_j] + 1
 
-				if result_in_new_direction < current_result {
-					result[cur_i][cur_j] = result_in_new_direction
-					queue = append(queue, []int{i, j + 1})
-					queue = append(queue, []int{i, j - 1})
-					queue = append(queue, []int{i + 1, j})
-					queue = append(queue, []int{i - 1, j})
+				if result_in_new_direction > current_result {
+					result[i][j] = current_result
+					queue = append(queue, []int{i, j})
+				}
+			}
+
+		}
+	}
+
+	return result
+}
+
+func updateMatrixSlow(mat [][]int) [][]int {
+	result := [][]int{}
+	queue := [][]int{}
+	for i := range mat {
+		i_result := []int{}
+		for j := range mat[i] {
+			if mat[i][j] == 0 {
+				i_result = append(i_result, 0)
+				queue = append(queue, []int{i, j})
+			} else {
+				i_result = append(i_result, len(mat))
+			}
+		}
+		result = append(result, i_result)
+	}
+
+	var current []int
+
+	for len(queue) > 0 {
+		current, queue = queue[0], queue[1:]
+		cur_i, cur_j := current[0], current[1]
+
+		directions := [][]int{
+			{cur_i, cur_j + 1},
+			{cur_i, cur_j - 1},
+			{cur_i + 1, cur_j},
+			{cur_i - 1, cur_j},
+		}
+		for _, direction := range directions {
+			i := direction[0]
+			j := direction[1]
+			if i >= 0 && j >= 0 && i < len(mat) && j < len(mat[i]) {
+				result_in_new_direction := result[i][j]
+				current_result := result[cur_i][cur_j] + 1
+
+				if result_in_new_direction > current_result {
+					result[i][j] = current_result
+					queue = append(queue, []int{i, j})
 				}
 			}
 
--- array/01-matrix/01-matrix_test.go
+++ array/01-matrix/01-matrix_test.go
@@ -2,7 +2,6 @@ package matrix_01
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -77,19 +76,17 @@ func TestExample5(t *testing.T) {
 
 func BenchmarkA(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		time.Sleep(1 * time.Second)
 		updateMatrix([][]int{
-			{1, 1},
-			{1, 0},
+			{1, 1, 0},
+			{1, 0, 0},
+			{0, 0, 0},
 		})
 	}
 }
 
 func BenchmarkB(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		time.Sleep(3 * time.Second)
-
-		updateMatrix([][]int{
+		updateMatrixSlow([][]int{
 			{1, 1, 0},
 			{1, 0, 0},
 			{0, 0, 0},
