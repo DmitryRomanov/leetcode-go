7f4e7dbaf16b26508b981622bbf9d67169a865c7
Dmitry Romanov <dmitry.romanov85@gmail.com>
2024-03-05 11:26:43 +0700

    try to resolve course-schedule-2

---
+++ graphs/course-schedule-2/course_schedule.go
@@ -0,0 +1,77 @@
+// https://leetcode.com/problems/course-schedule-ii/
+package course_schedule_2
+
+import (
+	"errors"
+)
+
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	if len(prerequisites) == 0 {
+		result := []int{}
+		for i := numCourses - 1; i >= 0; i-- {
+			result = append(result, i)
+		}
+		return result
+	}
+	digraph := getDigraph(prerequisites)
+	sorted, err := kahn(digraph)
+	if err != nil {
+		return []int{}
+	}
+	if len(sorted) < numCourses {
+		return []int{0}
+	}
+	return sorted[:numCourses]
+}
+
+// [][]int{{1, 0}, {2, 0}, {3, 1}, {3, 2}}
+func getDigraph(prerequisites [][]int) map[int][]int {
+	result := make(map[int][]int)
+	for _, v := range prerequisites {
+		vertex, root := v[0], v[1]
+		result[root] = append(result[root], vertex)
+	}
+	return result
+}
+
+// 0: [1, 2]
+// 1: [3]
+// 2: [3]
+// 3: []
+func kahn(digraph map[int][]int) ([]int, error) {
+	indegrees := make(map[int]int)
+	for u := range digraph {
+		if digraph[u] != nil {
+			for _, v := range digraph[u] {
+				indegrees[v]++
+			}
+		}
+	}
+
+	var queue []int
+	for u := range digraph {
+		if _, ok := indegrees[u]; !ok {
+			queue = append(queue, u)
+		}
+	}
+
+	var order []int
+	for len(queue) > 0 {
+		u := queue[len(queue)-1]
+		queue = queue[:(len(queue) - 1)]
+		order = append(order, u)
+		for _, v := range digraph[u] {
+			indegrees[v]--
+			if indegrees[v] == 0 {
+				queue = append(queue, v)
+			}
+		}
+	}
+
+	for _, indegree := range indegrees {
+		if indegree > 0 {
+			return order, errors.New("not a DAG")
+		}
+	}
+	return order, nil
+}
---
+++ graphs/course-schedule-2/course_schedule_test.go
@@ -0,0 +1,66 @@
+package course_schedule_2
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Explanation: There are a total of 2 courses to take.
+// To take course 1 you should have finished course 0. So the correct course order is [0,1].
+func TestExample1(t *testing.T) {
+	assert := assert.New(t)
+	result := findOrder(2, [][]int{{1, 0}})
+	assert.Equal([]int{0, 1}, result)
+}
+
+// Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2.
+// Both courses 1 and 2 should be taken after you finished course 0.
+// So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+func TestExample2(t *testing.T) {
+	assert := assert.New(t)
+	result := findOrder(4, [][]int{{1, 0}, {2, 0}, {3, 1}, {3, 2}})
+	assert.Equal([]int{0, 2, 1, 3}, result)
+}
+
+func TestExample4(t *testing.T) {
+	assert := assert.New(t)
+	result := findOrder(3, [][]int{{1, 0}, {2, 0}, {3, 1}, {3, 2}})
+	assert.Equal([]int{0, 2, 1}, result)
+}
+
+func TestExample5(t *testing.T) {
+	assert := assert.New(t)
+	result := findOrder(5, [][]int{{1, 0}, {2, 0}, {3, 1}, {3, 2}})
+	assert.Equal([]int{0}, result)
+}
+
+func TestExample7(t *testing.T) {
+	assert := assert.New(t)
+	result := findOrder(2, [][]int{{0, 1}, {1, 0}})
+	assert.Equal([]int{}, result)
+}
+
+func TestExample3(t *testing.T) {
+	assert := assert.New(t)
+	result := findOrder(1, [][]int{})
+	assert.Equal([]int{0}, result)
+}
+
+func TestExample6(t *testing.T) {
+	assert := assert.New(t)
+	result := findOrder(2, [][]int{})
+	assert.Equal([]int{1, 0}, result)
+}
+
+func TestExample8(t *testing.T) {
+	assert := assert.New(t)
+	result := findOrder(3, [][]int{})
+	assert.Equal([]int{2, 1, 0}, result)
+}
+
+func TestExample9(t *testing.T) {
+	assert := assert.New(t)
+	result := findOrder(3, [][]int{{1, 0}})
+	assert.Equal([]int{2, 0, 1}, result)
+}
