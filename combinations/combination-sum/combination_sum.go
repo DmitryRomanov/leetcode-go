8bec396e63046f2041563339383dd9a0758feea0
Dmitry Romanov <dmitry.romanov85@gmail.com>
2024-07-11 09:52:30 +0700

    another solve of coin-change

--- combinations/combination-sum/combination_sum.go
+++ combinations/combination-sum/combination_sum.go
@@ -1,65 +1,12 @@
 package combination_sum
 
-import "fmt"
-
 func combinationSum(candidates []int, target int) [][]int {
-	var result [][]int
-
-	maxElements := target / findMin(candidates)
-	variants := combrep(int(maxElements), candidates)
-
-	fmt.Println(variants)
-
-	for i := range variants {
-		variant := variants[i]
-		if check(variant, target) {
-			result = append(result, variant)
+	result := make([][]int, 0)
+	for _, candidate := range candidates {
+		if target-candidate >= 0 {
+			combinations := combinationSum(candidates, target-candidate)
+			result = append(result, combinations...)
 		}
 	}
-
 	return result
-}
-
-func removeDuplicateInt(intSlice []int) []int {
-	allKeys := make(map[int]bool)
-	list := []int{}
-	for _, item := range intSlice {
-		if _, value := allKeys[item]; !value {
-			allKeys[item] = true
-			list = append(list, item)
-		}
-	}
-	return list
-}
-
-func combrep(n int, lst []int) [][]int {
-	if n == 0 {
-		return [][]int{nil}
-	}
-	if len(lst) == 0 {
-		return nil
-	}
-	r := combrep(n, lst[1:])
-	for _, x := range combrep(n-1, lst) {
-		r = append(r, append(x, lst[0]))
-	}
-	return r
-}
-
-func check(lst []int, target int) bool {
-	sum := 0
-	for i := range lst {
-		sum += lst[i]
-	}
-	return sum == target
-}
-
-func findMin(a []int) (min int) {
-	min = a[0]
-	for _, value := range a {
-		if value < min {
-			min = value
-		}
-	}
-	return min
 }
--- dynamic/coin-change/coin_change.go
+++ dynamic/coin-change/coin_change.go
@@ -1,13 +1,6 @@
 package coin_change
 
-// []int{1, 2, 5}, 11
 func coinChange(coins []int, amount int) int {
-	dp := make([]int, amount+1)
-	for i := 0; i < amount+1; i++ {
-		dp[i] = amount + 1
-	}
-	dp[0] = 0
-
 	min := func(x, y int) int {
 		if x < y {
 			return x
@@ -15,8 +8,10 @@ func coinChange(coins []int, amount int) int {
 			return y
 		}
 	}
-
+	dp := make([]int, amount+1)
+	dp[0] = 0
 	for i := 1; i < amount+1; i++ {
+		dp[i] = amount + 1
 		for _, c := range coins {
 			if i-c >= 0 {
 				dp[i] = min(dp[i], dp[i-c]+1)
